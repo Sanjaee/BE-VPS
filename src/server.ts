3b924ea71f3de4290163edee827523c65f3d2020
Sanjaee <afrizaahmad18@gmail.com>
2025-03-15 21:57:28 +0700

    TEST

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use("/api/auth", authRoutes);
 
 // Route dasar untuk memastikan API berjalan
 app.get("/", (req, res) => {
-  res.json({ success: true, message: "AUTO DEPLOY!" });
+  res.json({ success: true, message: "API is running successfully!" });
 });
 
 const PORT: number = Number(process.env.PORT) || 3000;
