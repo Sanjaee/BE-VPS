725ded4680baf1aa5c60b08fca0ad8b50feef7a8
Sanjaee <afrizaahmad18@gmail.com>
2025-03-15 21:39:21 +0700

    test build

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import authRoutes from './routes/userRoute';
+import express from "express";
+import authRoutes from "./routes/userRoute";
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Route dasar untuk memastikan API berjalan
-app.get('/', (req, res) => {
-  res.json({ success: true, message: 'API is running successfully! test redeploy' });
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "API is running successfully!" });
 });
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
