4823f02615d09c35c0a0e36c81d10b71fd8d03e7
Sanjaee <afrizaahmad18@gmail.com>
2025-03-15 15:50:34 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,10 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
+
