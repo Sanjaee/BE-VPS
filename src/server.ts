8dd9fb2cc3781fd174fb2275b767409eaedbe373
Sanjaee <afrizaahmad18@gmail.com>
2025-03-15 16:15:08 +0700

    test github action

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,13 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+// Route dasar untuk memastikan API berjalan
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'API is running successfully!' });
+});
+
 const PORT: number = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
-
-
